68a72464f9c84b3baab701e64fe6e5aa468946da
JosiasAssamoi <assamoi.josias@gmail.com>
2020-12-10 08:31:28 +0100

    aws s3 init

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const main = async (): Promise<void> => {
     const port = argv[0] || (process.env.PORT as string)
     const host = argv[1] || (process.env.HOST as string)
     !fs.existsSync(myS3DATAPath) && fs.mkdirSync(myS3DATAPath)
-    console.log(myS3DATAPath);
+    console.log(` CHEMIN ==> ${myS3DATAPath}`);
     
     const server = new Server(host, parseInt(port, 10))
     await server.run()
